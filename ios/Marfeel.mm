f7c8ca14572a5c0618bf30693b033212a104dc70
Pir Shukarullah Shah Rashdi <shuker_rashdi@yahoo.com>
2025-10-23 13:28:49 +0400

    fix(ios): add Swift bridge layer for CompassSDK compatibility

--- ios/Marfeel.mm
+++ ios/Marfeel.mm
@@ -1,81 +1,70 @@
 #import "Marfeel.h"
-#import <MarfeelSDK_iOS/MarfeelSDK_iOS-Swift.h>
+#import "Marfeel-Swift.h"
 
 @implementation Marfeel
 
+RCT_EXPORT_MODULE()
+
 // Initialization
 RCT_EXPORT_METHOD(initialize:(NSString *)accountId
-                  pageTechnology:(NSNumber * _Nullable)pageTechnology)
+                  pageTechnology:(nullable NSNumber *)pageTechnology)
 {
     int accountIdInt = [accountId intValue];
-
-    if (pageTechnology != nil) {
-        [CompassTracker initializeWithAccountId:accountIdInt pageTechnology:[pageTechnology intValue] endpoint:nil];
-    } else {
-        [CompassTracker initializeWithAccountId:accountIdInt pageTechnology:nil endpoint:nil];
-    }
+    [MarfeelBridge initializeTrackerWithAccountId:accountIdInt pageTechnology:pageTechnology];
 }
 
 // Page & Screen Tracking
 RCT_EXPORT_METHOD(trackNewPage:(NSString *)url
-                  recirculationSource:(NSString * _Nullable)recirculationSource)
+                  recirculationSource:(nullable NSString *)recirculationSource)
 {
-    NSURL *nsUrl = [NSURL URLWithString:url];
-    [[CompassTracker shared] trackNewPageWithUrl:nsUrl rs:recirculationSource];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSURL *nsUrl = [NSURL URLWithString:url];
+        [MarfeelBridge trackNewPageWithUrl:nsUrl recirculationSource:recirculationSource];
+    });
 }
 
 RCT_EXPORT_METHOD(trackScreen:(NSString *)screen
-                  recirculationSource:(NSString * _Nullable)recirculationSource)
+                  recirculationSource:(nullable NSString *)recirculationSource)
 {
-    [[CompassTracker shared] trackScreenWithName:screen rs:recirculationSource];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [MarfeelBridge trackScreenWithName:screen recirculationSource:recirculationSource];
+    });
 }
 
 RCT_EXPORT_METHOD(stopTracking)
 {
-    [[CompassTracker shared] stopTracking];
+    [MarfeelBridge stopTracking];
 }
 
 RCT_EXPORT_METHOD(setLandingPage:(NSString *)landingPage)
 {
     NSURL *landingPageUrl = [NSURL URLWithString:landingPage];
-    [[CompassTracker shared] setLandingPageWithLandingPage:landingPageUrl];
+    [MarfeelBridge setLandingPageWithLandingPage:landingPageUrl];
 }
 
 // User Identification
 RCT_EXPORT_METHOD(setSiteUserId:(NSString *)userId)
 {
-    [[CompassTracker shared] setSiteUserIdWithUserId:userId];
+    [MarfeelBridge setSiteUserIdWithUserId:userId];
 }
 
 RCT_EXPORT_SYNCHRONOUS_TYPED_METHOD(NSString *, getUserId)
 {
-    return [[CompassTracker shared] getUserId];
+    return [MarfeelBridge getUserId];
 }
 
 // User Journey
 RCT_EXPORT_METHOD(setUserType:(NSString *)userType
-                  customId:(NSNumber * _Nullable)customId)
+                  customId:(nullable NSNumber *)customId)
 {
-    NSString *lowerUserType = [userType lowercaseString];
-
-    if ([lowerUserType isEqualToString:@"anonymous"]) {
-        [[CompassTracker shared] setUserType:UserTypeAnonymous];
-    } else if ([lowerUserType isEqualToString:@"logged"]) {
-        [[CompassTracker shared] setUserType:UserTypeLogged];
-    } else if ([lowerUserType isEqualToString:@"paid"]) {
-        [[CompassTracker shared] setUserType:UserTypePaid];
-    } else if ([lowerUserType isEqualToString:@"custom"] && customId != nil) {
-        [[CompassTracker shared] setUserType:[UserType customWithInt:[customId intValue]]];
-    } else {
-        [[CompassTracker shared] setUserType:UserTypeAnonymous];
-    }
+    [MarfeelBridge setUserTypeWithUserType:userType customId:customId];
 }
 
 // User RFV
 RCT_EXPORT_METHOD(getRFV:(RCTPromiseResolveBlock)resolve
                   reject:(RCTPromiseRejectBlock)reject)
 {
-    [[CompassTracker shared] getRFVWithCompletion:^(NSString * _Nonnull rfv) {
+    [MarfeelBridge getRFVWithCompletion:^(NSString * _Nonnull rfv) {
         resolve(rfv);
     }];
 }
@@ -83,62 +72,62 @@ RCT_EXPORT_METHOD(getRFV:(RCTPromiseResolveBlock)resolve
 // Conversions
 RCT_EXPORT_METHOD(trackConversion:(NSString *)conversion)
 {
-    [[CompassTracker shared] trackConversionWithConversion:conversion];
+    [MarfeelBridge trackConversionWithConversion:conversion];
 }
 
 // Consent
 RCT_EXPORT_METHOD(setConsent:(BOOL)hasConsent)
 {
-    [[CompassTracker shared] setConsentWithValue:hasConsent];
+    [MarfeelBridge setConsentWithValue:hasConsent];
 }
 
 // Custom Dimensions - Page Vars
 RCT_EXPORT_METHOD(setPageVar:(NSString *)name
                   value:(NSString *)value)
 {
-    [[CompassTracker shared] setPageVarWithName:name value:value];
+    [MarfeelBridge setPageVarWithName:name value:value];
 }
 
 // Custom Dimensions - Session Vars
 RCT_EXPORT_METHOD(setSessionVar:(NSString *)name
                   value:(NSString *)value)
 {
-    [[CompassTracker shared] setSessionVarWithName:name value:value];
+    [MarfeelBridge setSessionVarWithName:name value:value];
 }
 
 // Custom Dimensions - User Vars
 RCT_EXPORT_METHOD(setUserVar:(NSString *)name
                   value:(NSString *)value)
 {
-    [[CompassTracker shared] setUserVarWithName:name value:value];
+    [MarfeelBridge setUserVarWithName:name value:value];
 }
 
 // User Segments
 RCT_EXPORT_METHOD(addUserSegment:(NSString *)segment)
 {
-    [[CompassTracker shared] addUserSegmentWithSegment:segment];
+    [MarfeelBridge addUserSegmentWithSegment:segment];
 }
 
 RCT_EXPORT_METHOD(setUserSegments:(NSArray<NSString *> *)segments)
 {
-    [[CompassTracker shared] setUserSegmentsWithSegments:segments];
+    [MarfeelBridge setUserSegmentsWithSegments:segments];
 }
 
 RCT_EXPORT_METHOD(removeUserSegment:(NSString *)segment)
 {
-    [[CompassTracker shared] removeUserSegmentWithSegment:segment];
+    [MarfeelBridge removeUserSegmentWithSegment:segment];
 }
 
 RCT_EXPORT_METHOD(clearUserSegments)
 {
-    [[CompassTracker shared] clearUserSegments];
+    [MarfeelBridge clearUserSegments];
 }
 
 // Custom Metrics
 RCT_EXPORT_METHOD(setPageMetric:(NSString *)metric
                   value:(NSNumber *)value)
 {
-    [[CompassTracker shared] setPageMetricWithName:metric value:[value intValue]];
+    [MarfeelBridge setPageMetricWithName:metric value:[value intValue]];
 }
 
 // Multimedia Tracking
@@ -148,83 +137,20 @@ RCT_EXPORT_METHOD(initializeMultimediaItem:(NSString *)itemId
                   type:(NSString *)type
                   metadata:(NSDictionary *)metadata)
 {
-    MediaType *mediaType;
-    if ([[type lowercaseString] isEqualToString:@"audio"]) {
-        mediaType = MediaTypeAUDIO;
-    } else {
-        mediaType = MediaTypeVIDEO;
-    }
-
-    MultimediaMetadata *multimediaMetadata = [[MultimediaMetadata alloc] init];
-
-    if (metadata[@"isLive"] != nil) {
-        multimediaMetadata.isLive = [metadata[@"isLive"] boolValue];
-    }
-    if (metadata[@"title"] != nil) {
-        multimediaMetadata.title = metadata[@"title"];
-    }
-    if (metadata[@"description"] != nil) {
-        multimediaMetadata.description = metadata[@"description"];
-    }
-    if (metadata[@"url"] != nil) {
-        multimediaMetadata.url = [NSURL URLWithString:metadata[@"url"]];
-    }
-    if (metadata[@"thumbnail"] != nil) {
-        multimediaMetadata.thumbnail = metadata[@"thumbnail"];
-    }
-    if (metadata[@"authors"] != nil) {
-        multimediaMetadata.authors = metadata[@"authors"];
-    }
-    if (metadata[@"publishTime"] != nil) {
-        multimediaMetadata.publishTime = [metadata[@"publishTime"] longLongValue];
-    }
-    if (metadata[@"duration"] != nil) {
-        multimediaMetadata.duration = [metadata[@"duration"] intValue];
-    }
-
-    [[CompassTrackerMultimedia shared] initializeItemWithId:itemId
-                                                    provider:provider
-                                                  providerId:providerId
-                                                        type:mediaType
-                                                    metadata:multimediaMetadata];
+    [MarfeelBridge initializeMultimediaItemWithId:itemId
+                                          provider:provider
+                                        providerId:providerId
+                                              type:type
+                                          metadata:metadata];
 }
 
 RCT_EXPORT_METHOD(registerMultimediaEvent:(NSString *)itemId
                   event:(NSString *)event
                   eventTime:(NSNumber *)eventTime)
 {
-    NSString *lowerEvent = [event lowercaseString];
-    MediaEvent *mediaEvent;
-
-    if ([lowerEvent isEqualToString:@"play"]) {
-        mediaEvent = MediaEventPLAY;
-    } else if ([lowerEvent isEqualToString:@"pause"]) {
-        mediaEvent = MediaEventPAUSE;
-    } else if ([lowerEvent isEqualToString:@"end"]) {
-        mediaEvent = MediaEventEND;
-    } else if ([lowerEvent isEqualToString:@"updatecurrenttime"]) {
-        mediaEvent = MediaEventUPDATE_CURRENT_TIME;
-    } else if ([lowerEvent isEqualToString:@"adplay"]) {
-        mediaEvent = MediaEventAD_PLAY;
-    } else if ([lowerEvent isEqualToString:@"mute"]) {
-        mediaEvent = MediaEventMUTE;
-    } else if ([lowerEvent isEqualToString:@"unmute"]) {
-        mediaEvent = MediaEventUNMUTE;
-    } else if ([lowerEvent isEqualToString:@"fullscreen"]) {
-        mediaEvent = MediaEventFULL_SCREEN;
-    } else if ([lowerEvent isEqualToString:@"backscreen"]) {
-        mediaEvent = MediaEventBACK_SCREEN;
-    } else if ([lowerEvent isEqualToString:@"enterviewport"]) {
-        mediaEvent = MediaEventENTER_VIEWPORT;
-    } else if ([lowerEvent isEqualToString:@"leaveviewport"]) {
-        mediaEvent = MediaEventLEAVE_VIEWPORT;
-    } else {
-        mediaEvent = MediaEventPLAY;
-    }
-
-    [[CompassTrackerMultimedia shared] registerEventWithId:itemId
-                                                      event:mediaEvent
-                                                  eventTime:[eventTime intValue]];
+    [MarfeelBridge registerMultimediaEventWithId:itemId
+                                           event:event
+                                       eventTime:[eventTime intValue]];
 }
 
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
